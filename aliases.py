f9e226c59b0cf1f073ef046db83195e9003fd47d
Giulia Lavizzari <glavizza@lxplus738.cern.ch>
2023-11-16 17:28:56 +0100

    first working config

---
+++ aliases.py
@@ -0,0 +1,159 @@
+import os
+import copy
+import inspect
+
+configurations = os.path.realpath(inspect.getfile(inspect.currentframe())) # this file
+
+aliases = {}
+aliases = OrderedDict()
+
+bAlgo = 'DeepB'
+bWP = '0.4184'
+
+eleWP = 'mvaFall17V2Iso_WP90_SS_tthmva_70'
+muWP  = 'cut_Tight_HWWW_tthmva_80'
+
+mc = [skey for skey in samples if skey not in ('DATA', 'Fake_lep')]
+SSsamples = [skey for skey in mc if skey not in ('WW','DY','Higgs')] # 'Top' shoud be here
+OSsamples = [skey for skey in mc if skey in ('WW','DY','Higgs')]
+
+
+# -------- tau veto
+aliases['tauVeto_ww'] = {
+    'expr': '(Sum(Tau_pt > 18 && abs(Tau_eta)<2.3 && Tau_decayMode &&sqrt( pow(Tau_eta - Lepton_eta[0], 2) + pow(abs(abs(Tau_phi - Lepton_phi[0])-3.1415)-3.1415, 2) ) >= 0.4 && sqrt( pow(Tau_eta - Lepton_eta[1], 2) + pow(abs(abs(Tau_phi - Lepton_phi[1])-3.1415)-3.1415, 2) ) >= 0.4) == 0)'
+}
+
+# -------- lepton misidentification SF
+aliases['__chargeflip_w'] = {
+    'linesToAdd': ['#include "/afs/cern.ch/user/g/glavizza/private/mkShapesRDF/examples/WWSR/mischarge_sf.cc"\n'],
+    'samples': OSsamples
+}
+aliases['chargeflip_w'] = {
+    'expr' : 'misID_sf(nLepton,Lepton_pdgId,Lepton_pt,Lepton_eta)',
+    'samples': OSsamples
+}
+
+# -------- weights for VgS1
+aliases['gstarLow'] = {
+    'expr': 'Gen_ZGstar_mass >0 && Gen_ZGstar_mass < 4',
+    'samples': ['VgS1']
+}
+
+aliases['gstarHigh'] = {
+    'expr': 'Gen_ZGstar_mass <0 || Gen_ZGstar_mass > 4',
+    'samples': ['VgS1']
+}
+
+# -------- gen-matching to prompt only (GenLepMatch2l matches to *any* gen lepton)
+aliases['PromptGenLepMatch2l'] = {
+    'expr': 'Alt(Lepton_promptgenmatched,0,0)*Alt(Lepton_promptgenmatched,1,0)',
+    'samples': mc
+}
+aliases['PromptGenLepMatch3l'] = {
+     'expr': 'Alt(Lepton_promptgenmatched,0,0)*Alt(Lepton_promptgenmatched,1,0)*Alt(Lepton_promptgenmatched,2,0)',
+     'samples': mc
+ }
+aliases['PromptGenLepMatch4l'] = {
+    'expr': 'Alt(Lepton_promptgenmatched,0,0)*Alt(Lepton_promptgenmatched,1,0)*Alt(Lepton_promptgenmatched,2,0)*Alt(Lepton_promptgenmatched,3,0)',
+    'samples': mc
+}
+
+# -------- lepton WP
+aliases['LepWPCut'] = {
+    'expr': 'LepCut2l__ele_'+eleWP+'__mu_'+muWP,
+}
+
+# -------- fake lepton weights and variations
+aliases['fakeW'] = {
+    'expr': 'fakeW_ele_'+eleWP+'_mu_'+muWP+'_2l2j',
+    'samples': ['Fake_lep']
+}
+
+# ---------------------------- btagging (new)
+#loose 0.1241
+#medium 0.4184
+#tight 0.7527
+aliases['bVeto'] = {
+    'expr': '(Sum(CleanJet_pt > 20. && abs(CleanJet_eta) < 2.5 && Take(Jet_btagDeepB,CleanJet_jetIdx) > 0.4184) == 0)'
+}
+
+aliases['bReq'] = {
+    'expr': '(Sum(CleanJet_pt > 30. && abs(CleanJet_eta) < 2.5 && Take(Jet_btagDeepB,CleanJet_jetIdx) > 0.4184) >= 1)'
+}
+aliases['bVetoSF'] = {
+    'expr': 'TMath::Exp(Sum(LogVec((CleanJet_pt>20 && abs(CleanJet_eta)<2.5)*Take(Jet_btagSF_deepcsv_shape,CleanJet_jetIdx)+1*(CleanJet_pt<=20 || abs(CleanJet_eta)>=2.5))))',
+    'samples': mc
+}
+aliases['bReqSF'] = {
+    'expr': 'TMath::Exp(Sum(LogVec((CleanJet_pt>30 && abs(CleanJet_eta)<2.5)*Take(Jet_btagSF_deepcsv_shape,CleanJet_jetIdx)+1*(CleanJet_pt<=30 || abs(CleanJet_eta)>=2.5))))',
+    'samples': mc
+}
+aliases['btagSF'] = {
+    'expr': 'bVeto*bVetoSF + bReq*bReqSF',
+    'samples': mc
+}
+
+# ---------------------------- SFweight (new)
+aliases['SFweight_mod'] = {
+    'expr': ' * '.join(['XSWeight',
+                        'SFweight2l',
+                        'LepSF2l__ele_' + eleWP + '__mu_' + muWP, 
+                        'LepWPCut',
+                        'METFilter_MC']),
+    'samples': mc
+}
+
+aliases['samesign_requirement'] = {
+    'expr': '(Alt(Lepton_pdgId,0,-9999) * Alt(Lepton_pdgId,1,-9999) > 0)',
+    'samples':SSsamples
+}
+
+aliases['oppositesign_requirement'] = {
+    'expr': 'chargeflip_w*(Alt(Lepton_pdgId,0,-9999) * Alt(Lepton_pdgId,1,-9999) < 0)',
+    'samples':OSsamples
+}
+
+# ----------------------------
+
+
+
+
+
+########################################################################## 
+############### my own weights for the nuisances #########################
+
+#aliases['SFweightEleUp'] = {
+#    'expr': 'LepSF2l__ele_'+eleWP+'__Up',
+#    'samples': mc
+#}
+#aliases['SFweightEleDown'] = {
+#    'expr': 'LepSF2l__ele_'+eleWP+'__Do',
+#    'samples': mc
+#}
+#aliases['SFweightMuUp'] = {
+#    'expr': 'LepSF2l__mu_'+muWP+'__Up',
+#    'samples': mc
+#}
+#aliases['SFweightMuDown'] = {
+#    'expr': 'LepSF2l__mu_'+muWP+'__Do',
+#    'samples': mc
+#}
+#
+#aliases['Jet_PUIDSF'] = {
+#  'expr' : 'TMath::Exp(Sum((Jet_jetId>=2)*LogVec(Jet_PUIDSF_loose)))',
+#  'samples': mc
+#}
+#
+#aliases['Jet_PUIDSF_up'] = {
+#  'expr' : 'TMath::Exp(Sum((Jet_jetId>=2)*LogVec(Jet_PUIDSF_loose_up)))',
+#  'samples': mc
+#}
+#
+#aliases['Jet_PUIDSF_down'] = {
+#  'expr' : 'TMath::Exp(Sum((Jet_jetId>=2)*LogVec(Jet_PUIDSF_loose_down)))',
+#  'samples': mc
+#}
+
+
+
+
---
+++ samples.py
@@ -0,0 +1,317 @@
+import os, glob
+mcProduction = 'Summer20UL18_106x_nAODv9_Full2018v9'
+dataReco = 'Run2018_UL2018_nAODv9_Full2018v9'
+mcSteps = 'MCl1loose2018v9__MCCorr2018v9NoJERInHorn__l2tightOR2018v9'
+fakeSteps = 'DATAl1loose2018v9__l2loose__fakeW'
+dataSteps = 'DATAl1loose2018v9__l2loose__l2tightOR2018v9'
+
+##############################################
+###### Tree base directory for the site ######
+##############################################
+treeBaseDir = '/eos/cms/store/group/phys_higgs/cmshww/amassiro/HWWNano'
+limitFiles = -1
+
+
+def makeMCDirectory(var=''):
+        return os.path.join(treeBaseDir, mcProduction, mcSteps.format(var=''))
+
+mcDirectory = makeMCDirectory()
+fakeDirectory = os.path.join(treeBaseDir, dataReco, fakeSteps)
+dataDirectory = os.path.join(treeBaseDir, dataReco, dataSteps)
+
+samples = {}
+
+from mkShapesRDF.lib.search_files import SearchFiles
+searchFiles = SearchFiles()
+
+useXROOTD = False
+redirector = 'root://eoscms.cern.ch/'
+
+def nanoGetSampleFiles(path, name):
+    _files = searchFiles.searchFiles(path, name, redirector=redirector)
+    #_files = glob.glob(path + f"/nanoLatino_{name}__part*.root")
+    if limitFiles != -1 and len(_files) > limitFiles:
+        return [(name, _files[:limitFiles])]
+    else:
+        return  [(name, _files)]
+
+def CombineBaseW(samples, proc, samplelist):
+    _filtFiles = list(filter(lambda k: k[0] in samplelist, samples[proc]['name']))
+    _files = list(map(lambda k: k[1], _filtFiles))
+    _l = list(map(lambda k: len(k), _files))
+    leastFiles = _files[_l.index(min(_l))]
+    dfSmall = ROOT.RDataFrame("Runs", leastFiles)
+    s = dfSmall.Sum('genEventSumw').GetValue()
+    f = ROOT.TFile(leastFiles[0])
+    t = f.Get("Events")
+    t.GetEntry(1)
+    xs = t.baseW * s
+
+    __files = []
+    for f in _files:
+        __files += f
+    df = ROOT.RDataFrame("Runs", __files)
+    s = df.Sum('genEventSumw').GetValue()
+    newbaseW = str(xs / s)
+    weight = newbaseW + '/baseW'
+
+    for iSample in samplelist:
+        addSampleWeight(samples, proc, iSample, weight) 
+
+def addSampleWeight(samples, sampleName, sampleNameType, weight):
+    obj = list(filter(lambda k: k[0] == sampleNameType, samples[sampleName]['name']))[0]
+    samples[sampleName]['name'] = list(filter(lambda k: k[0] != sampleNameType, samples[sampleName]['name']))
+    if len(obj) > 2:
+        samples[sampleName]['name'].append((obj[0], obj[1], obj[2] + '*(' + weight + ')'))
+    else:
+        samples[sampleName]['name'].append((obj[0], obj[1], '(' + weight + ')' ))
+
+################################################
+############ DATA DECLARATION ##################
+################################################
+
+DataRun = [
+    ['A','Run2018A-UL2018-v1'],
+    ['B','Run2018B-UL2018-v1'],
+    ['C','Run2018C-UL2018-v1'],
+    ['D','Run2018D-UL2018-v1'],
+]
+
+DataSets = ['MuonEG','SingleMuon','EGamma','DoubleMuon']
+
+DataTrig = {
+    'MuonEG'         : 'Trigger_ElMu' ,
+    'DoubleMuon'     : '!Trigger_ElMu && Trigger_dblMu' ,
+    'SingleMuon'     : '!Trigger_ElMu && !Trigger_dblMu && Trigger_sngMu' ,
+    'EGamma'         : '!Trigger_ElMu && !Trigger_dblMu && !Trigger_sngMu && (Trigger_sngEl || Trigger_dblEl)' ,
+}
+
+################################################
+############ BASIC MC WEIGHTS ##################
+################################################
+
+Nlep='2'
+
+eleWP = 'mvaFall17V2Iso_WP90_SS_tthmva_70'
+muWP  = 'cut_Tight_HWWW_tthmva_80'
+
+LepWPCut        = 'LepCut'+Nlep+'l__ele_'+eleWP+'__mu_'+muWP
+LepWPweight     = 'LepSF'+Nlep+'l__ele_'+eleWP+'__mu_'+muWP
+
+METFilter_MC   = 'METFilter_MC'
+METFilter_DATA = 'METFilter_DATA'
+
+XSWeight      = 'XSWeight'
+SFweight      = 'SFweight'+Nlep+'l*'+LepWPweight+'*'+LepWPCut
+PromptGenLepMatch   = 'PromptGenLepMatch'+Nlep+'l'
+
+mcCommonWeight = 'SFweight_mod*btagSF*PromptGenLepMatch2l*samesign_requirement'
+mcCommonWeightNoMatch = 'SFweight_mod*btagSF*samesign_requirement'
+mcCommonWeightOS =  'SFweight_mod*btagSF*PromptGenLepMatch2l*oppositesign_requirement'
+# btag SF here bc maybe its different from the one of the central samples
+
+###########################################
+#############  BACKGROUNDS  ###############
+###########################################
+
+files = nanoGetSampleFiles(mcDirectory, 'WpWpJJ_EWK_UL')
+samples['SSWW'] = {
+    'name': files,
+    'weight': mcCommonWeight,
+    'FilesPerJob': 4
+}
+
+files = nanoGetSampleFiles(mcDirectory, 'WpWpJJ_QCD_UL')
+samples['WpWp_QCD'] = {
+    'name': files,
+    'weight': mcCommonWeight,
+    'FilesPerJob': 4
+}
+
+files = nanoGetSampleFiles(mcDirectory, 'WLLJJ_WToLNu_EWK_UL')
+samples['WZ_EWK'] = {
+    'name': files,
+    'weight': mcCommonWeight,
+    'FilesPerJob': 4
+}
+
+files = nanoGetSampleFiles(mcDirectory, 'WZTo3LNu')
+samples['WZ_QCD'] = {
+    'name': files,
+    'weight': mcCommonWeight,
+    'FilesPerJob': 4
+}
+
+
+samples['ZZ'] = {  'name'  :   nanoGetSampleFiles(mcDirectory,'ZZTo2L2Nu')
+                            # + nanoGetSampleFiles(directory,'ZZTo2L2Nu_ext2')
+                             + nanoGetSampleFiles(mcDirectory,'ZZTo2Q2L_mllmin4p0')
+                             + nanoGetSampleFiles(mcDirectory,'ZZTo4L'),
+                    'weight' :  XSWeight+'*'+SFweight+'*((nLepton==2)*PromptGenLepMatch2l + (nLepton==3)*PromptGenLepMatch3l + (nLepton>3)*PromptGenLepMatch4l)*'+METFilter_MC,
+                    'FilesPerJob' : 3,
+                 }
+
+#ZZ2LbaseW   = getBaseWnAOD(mcDirectory,'Summer20UL18_106x_nAODv9_Full2018v9',['ZZTo2L2Nu'])
+#ZZ4LbaseW   = getBaseWnAOD(mcDirectory,'Summer20UL18_106x_nAODv9_Full2018v9',['ZZTo4L'])
+#ggZZbaseW   = getBaseWnAOD(mcDirectory,'Summer20UL18_106x_nAODv9_Full2018v9',['ggZZ4m',        'ggZZ4m_ext1'])
+
+#addSampleWeight(samples,'ZZ','ZZTo2L2Nu',"1.07*"+ZZ2LbaseW+"/baseW") ## The non-ggZZ NNLO/NLO k-factor, cited from https://arxiv.org/abs/1405.2219v1 
+#addSampleWeight(samples,'ZZ','ZZTo2L2Q',      "1.07") 
+#addSampleWeight(samples,'ZZ','ZZTo4L',   "1.07*"+ZZ4LbaseW+"/baseW")
+
+
+files = nanoGetSampleFiles(mcDirectory, 'TTZToLLNuNu_M-10') + \
+        nanoGetSampleFiles(mcDirectory, 'TTWJetsToLNu')
+samples['TTV'] = {
+    'name': files,
+    'weight': mcCommonWeight,
+    'FilesPerJob': 4
+}
+files = nanoGetSampleFiles(mcDirectory, 'tZq_ll')
+samples['tZq'] = {
+    'name': files,
+    'weight': mcCommonWeight,
+    'FilesPerJob': 4
+}
+
+
+files = nanoGetSampleFiles(mcDirectory, 'ZGToLLG') + \
+        nanoGetSampleFiles(mcDirectory, 'WZTo3LNu_mllmin0p1')
+samples['VgS1'] = {
+    'name': files,
+    'weight': mcCommonWeight + ' * (gstarLow * 0.94 + gstarHigh * 1.14)',
+    'FilesPerJob': 4,
+    'subsamples': {
+        'L': 'gstarLow',
+        'H': 'gstarHigh'
+    }
+}
+#addSampleWeight(samples, 'VgS1', 'WGJJ', '(Gen_ZGstar_mass > 0 && Gen_ZGstar_mass < 0.1)')
+addSampleWeight(samples, 'VgS1', 'ZGToLLG', '(Gen_ZGstar_mass > 0)')
+addSampleWeight(samples, 'VgS1', 'WZTo3LNu_mllmin0p1', '(Gen_ZGstar_mass > 0.1 && Gen_ZGstar_mass<4)')
+
+### Wrong-sign
+files = nanoGetSampleFiles(mcDirectory, 'WWTo2L2Nu') + \
+        nanoGetSampleFiles(mcDirectory, 'GluGluToWWToENEN') + \
+        nanoGetSampleFiles(mcDirectory, 'GluGluToWWToENMN') + \
+        nanoGetSampleFiles(mcDirectory, 'GluGluToWWToENTN') + \
+        nanoGetSampleFiles(mcDirectory, 'GluGluToWWToMNEN') + \
+        nanoGetSampleFiles(mcDirectory, 'GluGluToWWToMNMN') + \
+        nanoGetSampleFiles(mcDirectory, 'GluGluToWWToMNTN') + \
+        nanoGetSampleFiles(mcDirectory, 'GluGluToWWToTNEN') + \
+        nanoGetSampleFiles(mcDirectory, 'GluGluToWWToTNMN') + \
+        nanoGetSampleFiles(mcDirectory, 'GluGluToWWToTNTN')
+samples['WW'] = {
+    'name': files,
+    'weight': mcCommonWeightOS,
+    'FilesPerJob': 17,
+}
+
+#files = nanoGetSampleFiles(mcDirectory, 'TTTo2L2Nu') + \
+#        nanoGetSampleFiles(mcDirectory, 'ST_tW_top') + \
+#        nanoGetSampleFiles(mcDirectory, 'ST_tW_antitop')
+#samples['Top'] = {
+#    'name': files,
+#    'weight': mcCommonWeightOS,
+#    'FilesPerJob': 3,
+#}
+
+ptllDYW_NLO = '(0.87*(gen_ptll<10)+(0.379119+0.099744*gen_ptll-0.00487351*gen_ptll**2+9.19509e-05*gen_ptll**3-6.0212e-07*gen_ptll**4)*(gen_ptll>=10 && gen_ptll<45)+(9.12137e-01+1.11957e-04*gen_ptll-3.15325e-06*gen_ptll**2-4.29708e-09*gen_ptll**3+3.35791e-11*gen_ptll**4)*(gen_ptll>=45 && gen_ptll<200) + 1*(gen_ptll>200))'
+ptllDYW_LO = '((0.632927+0.0456956*gen_ptll-0.00154485*gen_ptll*gen_ptll+2.64397e-05*gen_ptll*gen_ptll*gen_ptll-2.19374e-07*gen_ptll*gen_ptll*gen_ptll*gen_ptll+6.99751e-10*gen_ptll*gen_ptll*gen_ptll*gen_ptll*gen_ptll)*(gen_ptll>0)*(gen_ptll<100)+(1.41713-0.00165342*gen_ptll)*(gen_ptll>=100)*(gen_ptll<300)+1*(gen_ptll>=300))'
+files = nanoGetSampleFiles(mcDirectory, 'DYJetsToLL_M-50') + \
+        nanoGetSampleFiles(mcDirectory, 'DYJetsToLL_M-10to50-LO')
+samples['DY'] = {
+    'name': files,
+    'weight':  mcCommonWeightOS+'*( !(Sum(PhotonGen_isPrompt==1 && PhotonGen_pt>15 && abs(PhotonGen_eta)<2.6) > 0 &&\
+        Sum(LeptonGen_isPrompt==1 && LeptonGen_pt>15)>=2) )',
+    'FilesPerJob': 3,
+}
+
+files = nanoGetSampleFiles(mcDirectory, 'GluGluHToWWTo2L2Nu_M125') + \
+        nanoGetSampleFiles(mcDirectory, 'GluGluHToTauTau_M125') + \
+        nanoGetSampleFiles(mcDirectory, 'VBFHToWWTo2L2Nu_M125') + \
+        nanoGetSampleFiles(mcDirectory, 'VBFHToTauTau_M125') + \
+        nanoGetSampleFiles(mcDirectory, 'ttHToNonbb_M125')
+samples['Higgs'] = {
+    'name': files,
+    'weight': mcCommonWeightOS,
+    'FilesPerJob': 17,
+}
+
+
+files = nanoGetSampleFiles(mcDirectory, 'ZZZ') + \
+        nanoGetSampleFiles(mcDirectory, 'WZZ') + \
+        nanoGetSampleFiles(mcDirectory, 'WWZ') + \
+        nanoGetSampleFiles(mcDirectory, 'WWW') + \
+        nanoGetSampleFiles(mcDirectory, 'WWG')
+samples['VVV'] = {
+    'name': files,
+    'weight': mcCommonWeight,
+    'FilesPerJob': 4
+}
+
+###########################################
+################## FAKE ###################
+###########################################
+
+samples['Fake_lep'] = {
+  'name': [],
+  'weight': 'METFilter_DATA*fakeW',
+  'weights': [],
+  'isData': ['all'],
+  'FilesPerJob': 20
+}
+for _, sd in DataRun:
+  for pd in DataSets:
+    datatag = pd + '_' + sd
+
+    if (   ('DoubleMuon' in pd and 'Run2018B' in sd)
+        or ('DoubleMuon' in pd and 'Run2018D' in sd)
+        or ('SingleMuon' in pd and 'Run2018A' in sd)
+        or ('SingleMuon' in pd and 'Run2018B' in sd)
+        or ('SingleMuon' in pd and 'Run2018C' in sd)):
+        print("sd      = {}".format(sd))
+        print("pd      = {}".format(pd))
+        print("Old datatag = {}".format(datatag))
+        datatag = datatag.replace('v1','v2')
+        print("New datatag = {}".format(datatag))
+    files = nanoGetSampleFiles(fakeDirectory, datatag)
+
+    samples['Fake_lep']['name'].extend(files)
+    addSampleWeight(samples, 'Fake_lep', datatag, DataTrig[pd])
+
+
+###########################################
+################## DATA ###################
+###########################################
+
+samples['DATA'] = {
+  'name': [],
+  'weight': 'LepWPCut*METFilter_DATA',
+  'weights': [],
+  'isData': ['all'],
+  'FilesPerJob': 50
+}
+
+for _, sd in DataRun:
+  for pd in DataSets:
+    datatag = pd + '_' + sd
+
+    if (   ('DoubleMuon' in pd and 'Run2018B' in sd)
+        or ('DoubleMuon' in pd and 'Run2018D' in sd)
+        or ('SingleMuon' in pd and 'Run2018A' in sd)
+        or ('SingleMuon' in pd and 'Run2018B' in sd)
+        or ('SingleMuon' in pd and 'Run2018C' in sd)):
+        print("sd      = {}".format(sd))
+        print("pd      = {}".format(pd))
+        print("Old datatag = {}".format(datatag))
+        datatag = datatag.replace('v1','v2')
+        print("New datatag = {}".format(datatag))
+
+    files = nanoGetSampleFiles(dataDirectory, datatag)
+
+    samples['DATA']['name'].extend(files)
+    addSampleWeight(samples, 'DATA', datatag, DataTrig[pd])
+
+
+# samples = {k:v for k,v in samples.items() if k in ['Fake_lep']}
